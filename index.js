9135c4a25f4edfb766b2012f2da71ad731584510
Vaibhav Soni <soni89vaibhav@gmail.com>
2023-06-04 03:22:16 +0530

    Add files via upload
    These are first commit of this work

---
+++ index.js
@@ -0,0 +1,48 @@
+document.getElementById("PaintingDiv").style.display = "none";
+    document.getElementById("AbstactDiv").style.display = "none";
+    document.getElementById("nudetDiv").style.display = "none";
+    document.getElementById("contactUsDiv").style.display = "none";
+
+function contactUs() {
+    //  alert('AboutUs clicked');
+    document.getElementById("mainDiv").style.display = "none";
+    document.getElementById("PaintingDiv").style.display = "none";
+    document.getElementById("AbstactDiv").style.display = "none";
+    document.getElementById("nudetDiv").style.display = "none";
+    document.getElementById("contactUsDiv").style.display = "block";
+}
+
+function main() {
+    //  alert('AboutUs clicked');
+    document.getElementById("mainDiv").style.display = "block";
+    document.getElementById("PaintingDiv").style.display = "none";
+    document.getElementById("AbstactDiv").style.display = "none";
+    document.getElementById("nudetDiv").style.display = "none";
+    document.getElementById("contactUsDiv").style.display = "none";
+}
+
+function Painting() {
+    //  alert('AboutUs clicked');
+    document.getElementById("mainDiv").style.display = "none";
+    document.getElementById("PaintingDiv").style.display = "block";
+    document.getElementById("AbstactDiv").style.display = "none";
+    document.getElementById("nudetDiv").style.display = "none";
+    document.getElementById("contactUsDiv").style.display = "none";
+}
+function Abstract() {
+    //  alert('AboutUs clicked');
+    document.getElementById("mainDiv").style.display = "none";
+    document.getElementById("PaintingDiv").style.display = "none";
+    document.getElementById("AbstactDiv").style.display = "block";
+    document.getElementById("nudetDiv").style.display = "none";
+    document.getElementById("contactUsDiv").style.display = "none";
+}
+function Nude() {
+    //  alert('AboutUs clicked');
+    document.getElementById("mainDiv").style.display = "none";
+    document.getElementById("PaintingDiv").style.display = "none";
+    document.getElementById("AbstactDiv").style.display = "none";
+    document.getElementById("nudetDiv").style.display = "block";
+    document.getElementById("contactUsDiv").style.display = "none";
+}
+
